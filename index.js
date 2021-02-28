73030100b60bee9d7c9292027c3f3e1a47ba042f
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-02-27 23:16:52 -0600

    commit tree a26d465fc5eb919e3a1bb237b76d77ce7ad8725f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map((num) => {return num * -1});
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map((num) => {return num});
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map((num) => {return num * 2});
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map((num) => {return num * num});
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return sourceArray.reduce(reducer, startingPoint);
+}
+
+function reduceToAllTrue(sourceArray) {
+    const reducer = (accumulator, currentValue) => {
+        if(!!currentValue) {
+            return accumulator = true;
+        } else {
+            return accumulator = false;
+        }};
+    return sourceArray.reduce(reducer);
+}
+
+function reduceToAnyTrue(sourceArray) {
+    const reducer = (accumulator, currentValue) => {
+        if(!!currentValue) {
+            return accumulator = true;
+        } else {
+            return accumulator = false;
+        }};
+    return sourceArray.reduce(reducer);
+}
